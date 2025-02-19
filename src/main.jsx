f6aab20bfd29e5b232605031b0feefd0feec07e6
tanermaddocks <maddockstaner@gmail.com>
2025-02-19 18:55:32 +1000

    feat: skeletion file and directory structure set up for pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+    
+            <h1>Cool global content</h1>
+            <Routes>
+
+              <Route path='/' element={<h1>Homepage route content</h1>} />
+              <Route path='/about' element={<h1>About Page route content</h1>} />
+              <Route path='/contact' element={<h1>Contact Page route content</h1>} />
+              <Route path='/projects' element={<h1>Project Page route content</h1>} />
+
+            </Routes>
+
+    </BrowserRouter>
   </StrictMode>,
 )
